5063da22b325bf2fc16311604cc12017f9ddae66
Rauan2308 <rauan.kyrykbayev@gmail.com>
2024-10-05 19:52:53 +1000

    Add my files

---
+++ inventory_management/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://db_postgresql_k4h6_user:yvSo36ZlE57JYk9NhFbG2kwzpfqj9F1S@dpg-crc0frbv2p9s73dl0akg-a.oregon-postgres.render.com/db_postgresql_k4h6')
---
+++ scripts/build-image.sh
@@ -0,0 +1,19 @@
+#
+# Builds a Docker image.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   VERSION - The version number to tag the images with.
+#
+# Usage:
+#
+#       ./scripts/build-image.sh
+#
+
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+docker build -t $CONTAINER_REGISTRY/book_catalog:$VERSION ./book_catalog
+docker build -t $CONTAINER_REGISTRY/inventory_management:$VERSION ./inventory_management
