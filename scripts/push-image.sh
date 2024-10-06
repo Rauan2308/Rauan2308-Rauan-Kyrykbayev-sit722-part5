9d63fbd73b59b30e0a23e2952d36a2a3b2243fe1
Rauan2308 <rauan.kyrykbayev@gmail.com>
2024-10-06 14:05:40 +1100

    test

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -17,6 +17,6 @@ set -u # or set -o nounset
 : "$VERSION"
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN -p
 docker push $CONTAINER_REGISTRY/book_catalog:$VERSION
 docker push $CONTAINER_REGISTRY/inventory_management:$VERSION
